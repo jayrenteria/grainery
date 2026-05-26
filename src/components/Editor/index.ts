0eabd03543d1c5a5b0dfff6bc7b396989cd8c82e
jayrenteria <jayevanrenteria@gmail.com>
2026-05-26 16:35:12 -0600

    Adds subtle styling difference

--- src/components/Editor/index.ts
+++ src/components/Editor/index.ts
@@ -1,5 +1,6 @@
 export { ScreenplayEditor } from './ScreenplayEditor';
 export { ElementTypeIndicator } from './ElementTypeIndicator';
+export { EditorStats } from './EditorStats';
 export { TitlePageEditor } from './TitlePageEditor';
 export { PaginatedEditor } from './PaginatedEditor';
 export { FindReplaceBar } from './FindReplaceBar';
